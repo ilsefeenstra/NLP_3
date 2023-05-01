edc5a64b835e2fabe4cf41255ad513d499e0ea23
ilsefeenstra <ilsef@me.com>
2023-05-01 15:04:42 +0200

    start

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# NLP_3
-NLP assignment 3
+# OpenAI API Quickstart - Python example app
+
+This is an example pet name generator app used in the OpenAI API [quickstart tutorial](https://beta.openai.com/docs/quickstart). It uses the [Flask](https://flask.palletsprojects.com/en/2.0.x/) web framework. Check out the tutorial or follow the instructions below to get set up.
+
+## Setup
+
+1. If you don’t have Python installed, [install it from here](https://www.python.org/downloads/).
+
+2. Clone this repository.
+
+3. Navigate into the project directory:
+
+   ```bash
+   $ cd NLP_assignment_3
+   ```
+
+4. Create a new virtual environment:
+
+   ```bash
+   $ python -m venv venv
+   $ . venv/bin/activate
+   ```
+
+5. Install the requirements:
+
+   ```bash
+   $ pip install -r requirements.txt
+   ```
+
+6. Make a copy of the example environment variables file:
+
+   ```bash
+   $ cp .env.example .env
+   ```
+
+7. Add your [API key](https://beta.openai.com/account/api-keys) to the newly created `.env` file.
+
+8. Run the app:
+
+   ```bash
+   $ flask run
+   ```
+
+You should now be able to access the app at [http://localhost:5000](http://localhost:5000)! 
+
+9. Type in a prompt: this can be any subject. If you don't type a prompt and click an generate, a question with a random subject is created. 
---
+++ app.py
@@ -0,0 +1,91 @@
+# import os
+
+# import openai
+# from flask import Flask, redirect, render_template, request, url_for
+
+# app = Flask(__name__)
+# #openai.api_key = os.getenv("OPENAI_API_KEY")
+# openai.api_key = ("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")
+
+
+# @app.route("/", methods=("GET", "POST"))
+# def index():
+#     if request.method == "POST":
+#         animal = request.form["animal"]
+#         response = openai.Completion.create(
+#             model="text-davinci-003",
+#             prompt=generate_prompt(animal),
+#             temperature=0.6,
+#         )
+#         return redirect(url_for("index", result=response.choices[0].text))
+
+#     result = request.args.get("result")
+#     return render_template("index.html", result=result)
+
+
+# def generate_prompt(animal):
+#     return """Suggest three names for an animal that is a superhero.
+
+# Animal: Cat
+# Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+# Animal: Dog
+# Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+# Animal: {}
+# Names:""".format(
+#         animal.capitalize()
+#     )
+
+from flask import Flask, render_template, request, redirect, url_for
+import openai
+import random
+import os
+
+# Apply your OpenAI API key here
+openai.api_key = ("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")
+#openai.api_key = os.getenv("OPENAI_API_KEY")
+app = Flask(__name__)
+
+def generate_coffee_talk_starters(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=f"Create a coffee talk starter question based on the subject: {prompt}",
+        max_tokens=100,
+        n=1,
+        stop=None,
+        temperature=0.8,
+    )
+    question = response.choices[0].text.strip()
+    return question
+
+def generate_random_subject():
+    subjects = [
+        "movies",
+        "music",
+        "sports",
+        "hobbies",
+        "travel",
+        "technology",
+        "food",
+        "books",
+        "work-life balance",
+        "career",
+    ]
+    return random.choice(subjects)
+
+@app.route("/", methods=("GET", "POST"))
+def index():
+    try:
+        if request.method == "POST":
+            subject = request.form["subject"]
+            if not subject:
+                subject = generate_random_subject()
+            question = generate_coffee_talk_starters(subject)
+            return redirect(url_for("index", result=question))
+
+        result = request.args.get("result")
+        return render_template("index.html", result=result)
+    except Exception as e:
+        return render_template("error.html", error_message=str(e))
+
+if __name__ == "__main__":
+    app.run(debug=True)
