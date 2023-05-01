24a5647fa8806203d35f1fece7dd89336c73cedb
ilsefeenstra <ilsef@me.com>
2023-05-01 19:27:27 +0200

    add webscraping

--- app.py
+++ app.py
@@ -39,9 +39,11 @@ from flask import Flask, render_template, request, redirect, url_for
 import openai
 import random
 import os
+import requests
+from bs4 import BeautifulSoup
 
 # Apply your OpenAI API key here
-openai.api_key = ("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")
+openai.api_key = ("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")
 #openai.api_key = os.getenv("OPENAI_API_KEY")
 app = Flask(__name__)
 
@@ -52,24 +54,51 @@ def generate_coffee_talk_starters(prompt):
         max_tokens=100,
         n=1,
         stop=None,
-        temperature=0.8,
+        temperature=0.9,
     )
     question = response.choices[0].text.strip()
     return question
 
+# def generate_random_subject():
+#     subjects = [
+#         "movies",
+#         "music",
+#         "sports",
+#         "hobbies",
+#         "travel",
+#         "technology",
+#         "food",
+#         "books",
+#         "work-life balance",
+#         "career",
+#     ]
+#     return random.choice(subjects)
+
 def generate_random_subject():
-    subjects = [
-        "movies",
-        "music",
-        "sports",
-        "hobbies",
-        "travel",
-        "technology",
-        "food",
-        "books",
-        "work-life balance",
-        "career",
-    ]
+    url = 'https://conversationstartersworld.com/good-questions-to-ask/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    subjects = []
+
+    for li in soup.select('ol li'):
+        subjects.append(li.text.strip())
+
+    # If the list is empty or shorter than expected, use a fallback list of subjects
+    if len(subjects) == 0:
+        subjects = [
+            "Travel",
+            "Hobbies",
+            "Movies and TV Shows",
+            "Work and Career",
+            "Books",
+            "Technology",
+            "Food and Drink",
+            "Sports",
+            "Music",
+            "Current Events",
+        ]
+
     return random.choice(subjects)
 
 @app.route("/", methods=("GET", "POST"))
@@ -88,4 +117,4 @@ def index():
         return render_template("error.html", error_message=str(e))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
