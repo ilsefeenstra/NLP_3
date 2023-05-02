d0b86019c50e9620978fb8444abef7addf51e107
ilsefeenstra <ilsef@me.com>
2023-05-02 12:01:32 +0200

    clean up code

--- app.py
+++ app.py
@@ -1,40 +1,3 @@
-# import os
-
-# import openai
-# from flask import Flask, redirect, render_template, request, url_for
-
-# app = Flask(__name__)
-# #openai.api_key = os.getenv("OPENAI_API_KEY")
-# openai.api_key = ("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")
-
-
-# @app.route("/", methods=("GET", "POST"))
-# def index():
-#     if request.method == "POST":
-#         animal = request.form["animal"]
-#         response = openai.Completion.create(
-#             model="text-davinci-003",
-#             prompt=generate_prompt(animal),
-#             temperature=0.6,
-#         )
-#         return redirect(url_for("index", result=response.choices[0].text))
-
-#     result = request.args.get("result")
-#     return render_template("index.html", result=result)
-
-
-# def generate_prompt(animal):
-#     return """Suggest three names for an animal that is a superhero.
-
-# Animal: Cat
-# Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-# Animal: Dog
-# Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-# Animal: {}
-# Names:""".format(
-#         animal.capitalize()
-#     )
-
 from flask import Flask, render_template, request, redirect, url_for
 import openai
 import random
@@ -43,11 +6,13 @@ import requests
 from bs4 import BeautifulSoup
 
 # Apply your OpenAI API key here
-openai.api_key = ("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")
-#openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = ("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")
 app = Flask(__name__)
 
 def generate_coffee_talk_starters(prompt):
+    """
+    This function generates a coffee talk starter question based on a given subject
+    """
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=f"Create a coffee talk starter question based on the subject: {prompt}",
@@ -59,22 +24,11 @@ def generate_coffee_talk_starters(prompt):
     question = response.choices[0].text.strip()
     return question
 
-# def generate_random_subject():
-#     subjects = [
-#         "movies",
-#         "music",
-#         "sports",
-#         "hobbies",
-#         "travel",
-#         "technology",
-#         "food",
-#         "books",
-#         "work-life balance",
-#         "career",
-#     ]
-#     return random.choice(subjects)
 
 def generate_random_subject():
+    """
+    This function generates a random subject by scraping a website
+    """
     url = 'https://conversationstartersworld.com/good-questions-to-ask/'
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
@@ -101,20 +55,27 @@ def generate_random_subject():
 
     return random.choice(subjects)
 
+# Define the route for the main page, handling both GET and POST requests
 @app.route("/", methods=("GET", "POST"))
 def index():
     try:
+        # If the request is a POST request, generate a question based on the input subject
         if request.method == "POST":
             subject = request.form["subject"]
+            
             if not subject:
                 subject = generate_random_subject()
             question = generate_coffee_talk_starters(subject)
             return redirect(url_for("index", result=question))
-
+        
+        # If the request is a GET request, display the main page with the generated question (if any)
         result = request.args.get("result")
         return render_template("index.html", result=result)
+    
     except Exception as e:
+        # If an error occurs, display the error page with the error message
         return render_template("error.html", error_message=str(e))
 
+# Start the Flask app
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
